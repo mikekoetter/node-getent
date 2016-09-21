fa4d61ab5eb203693b5b03c883c7ef7d625b7fee
mikekoetter <mike.koetter@gmail.com>
2016-09-21 13:53:51 -0700

    node-pre-gyp build for solaris

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /** Node.js getent Library */
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var internal = require(binding_path);
 
-var internal =  require('./build/Release/getent');
 var databases = {};
 
 /* Implement passwd database search */
